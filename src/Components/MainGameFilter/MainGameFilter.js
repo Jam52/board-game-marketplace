11191832753fc1ac6528b6031c436443facc90bf
Jam52 <jamie.sajdak@gmail.com>
2020-11-17 14:16:24 -0500

    Add styles to MainGameFilter

--- src/Components/MainGameFilter/MainGameFilter.js
+++ src/Components/MainGameFilter/MainGameFilter.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from '../../Components/SearchBar/SearchBar';
 import { fetchDropdownOptions } from '../../services/boardgameApi';
+import classes from './MainGameFilter.module.scss';
 
 class MainGameFilter extends Component {
   state = {
@@ -57,70 +58,108 @@ class MainGameFilter extends Component {
       });
     }
     return (
-      <form data-testid="component-main-game-filter">
-        <div>
-          <label htmlFor="categories">Add a category</label>
-          <select data-testid="categories-dropdown" id="categories">
-            <option>Categories</option>
-            {categoryOptions}
-          </select>
-          <SearchBar
-            testid="category-search"
-            valid={this.state.categories}
-            for="categories"
-            placeholder="Category Search"
-            submit={this.submitSearchHandler}
-          />
+      <form data-testid="component-main-game-filter" className={classes.form}>
+        <div className={classes.mainSearch}>
+          <div className={classes.mainSearch_section}>
+            <label htmlFor="categories">Add a category</label>
+            <div className={classes.mainSearch_inputs}>
+              <select
+                data-testid="categories-dropdown"
+                id="categories"
+                className={classes.mainSearch_dropdown}
+              >
+                <option>Categories</option>
+                {categoryOptions}
+              </select>
+              <SearchBar
+                testid="category-search"
+                valid={this.state.categories}
+                for="categories"
+                placeholder="Category Search"
+                submit={this.submitSearchHandler}
+              />
+            </div>
+          </div>
+
+          <div className={classes.mainSearch_section}>
+            <label htmlFor="mechanics">Add a Mechnic</label>
+            <div className={classes.mainSearch_inputs}>
+              <select
+                data-testid="mechanics-dropdown"
+                id="mechanics"
+                className={classes.mainSearch_dropdown}
+              >
+                <option>Mechanics</option>
+                {mechanicsOptions}
+              </select>
+              <SearchBar
+                testid="mechanic-search"
+                valid={this.state.mechanics}
+                for="mechanics"
+                placeholder="Mechanic Search"
+              />
+            </div>
+          </div>
         </div>
-        <div>
-          <label htmlFor="mechanics">Add a Mechnic</label>
-          <select data-testid="mechanics-dropdown" id="mechanics">
-            <option>Mechanics</option>
-            {mechanicsOptions}
-          </select>
-          <SearchBar
-            testid="mechanic-search"
-            valid={this.state.mechanics}
-            for="mechanics"
-            placeholder="Mechanic Search"
-          />
-        </div>
-        <div>
-          <label htmlFor="player count">Max Player Count</label>
-          <select id="player count" data-testid="player-count-dropdown">
-            <option value="1">Single Player</option>
-            <option value="2">2 Players</option>
-            <option value="4">4 Players</option>
-            <option value="7">7 Players</option>
-            <option value="7+">7+ Players</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="play time">Play Time</label>
-          <select id="play time" data-testid="play-time-dropdown">
-            <option value="30">30 Mins</option>
-            <option value="60">60 Mins</option>
-            <option value="90">90 Mins</option>
-            <option value="120">120 Mins</option>
-            <option value="120+">120+ Mins</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="year published">Year Published</label>
-          <input
-            type="number"
-            placeholder="enter a date"
-            data-testid="year-published-input"
-          />
-        </div>
-        <div>
-          <label htmlFor="order by">Order By</label>
-          <select id="order by" data-testid="order-by-dropdown">
-            <option value="popularity">Popularity</option>
-            <option value="price">Price</option>
-            <option value="year_published">Year Published</option>
-            <option value="max_playtime">Play Time</option>
-          </select>
+        <div className={classes.subSearch}>
+          <div className={classes.subSearch_section}>
+            <label className={classes.subSearch_label} htmlFor="player count">
+              Max Player Count
+            </label>
+            <select
+              id="player count"
+              data-testid="player-count-dropdown"
+              className={classes.subSearch_dropdown}
+            >
+              <option value="1">Single Player</option>
+              <option value="2">2 Players</option>
+              <option value="4">4 Players</option>
+              <option value="7">7 Players</option>
+              <option value="7+">7+ Players</option>
+            </select>
+          </div>
+          <div className={classes.subSearch_section}>
+            <label className={classes.subSearch_label} htmlFor="play time">
+              Play Time
+            </label>
+            <select
+              id="play time"
+              data-testid="play-time-dropdown"
+              className={classes.subSearch_dropdown}
+            >
+              <option value="30">30 Mins</option>
+              <option value="60">60 Mins</option>
+              <option value="90">90 Mins</option>
+              <option value="120">120 Mins</option>
+              <option value="120+">120+ Mins</option>
+            </select>
+          </div>
+          <div className={classes.subSearch_section}>
+            <label className={classes.subSearch_label} htmlFor="year published">
+              Year Published
+            </label>
+            <input
+              type="number"
+              placeholder="enter a date"
+              data-testid="year-published-input"
+              className={classes.subSearch_input}
+            />
+          </div>
+          <div className={classes.subSearch_section}>
+            <label className={classes.subSearch_label} htmlFor="order by">
+              Order By
+            </label>
+            <select
+              id="order by"
+              data-testid="order-by-dropdown"
+              className={classes.subSearch_dropdown}
+            >
+              <option value="popularity">Popularity</option>
+              <option value="price">Price</option>
+              <option value="year_published">Year Published</option>
+              <option value="max_playtime">Play Time</option>
+            </select>
+          </div>
         </div>
       </form>
     );
